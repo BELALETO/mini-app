987d6b37cbca35e13e0ea748aa80ea5efaefdf21
Belal Sryo <belalsryo@gmail.com>
2025-11-30 18:27:08 +0200

    Refactor global error handler to utilize AppError for improved error handling

--- src/controllers/globalErrorHandler.js
+++ src/controllers/globalErrorHandler.js
@@ -1,14 +1,28 @@
+const AppError = require('../utils/appError');
+
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
-  const error = new Error(message);
-  error.statusCode = 400;
-  error.status = 'fail';
-  error.isOperational = true;
+  const error = new AppError(message, 400);
   return error;
 };
 
+const handleValidationError = (err) => {
+  let message = '';
+  for (const key in err.errors) {
+    const errorObj = err.errors[key];
+    message += `${errorObj.message} `;
+  }
+  const error = new AppError(message, 400);
+  return error;
+};
+
+const handleDuplicateFields = (err) => {
+  const field = Object.keys(err.keyValue)[0];
+  const message = `${field} is a duplicate field`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
-  if (err.name === 'CastError') err = handleCastErrorDB(err);
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
@@ -27,20 +41,27 @@ const sendErrorProd = (err, res) => {
     console.error('Error💥', err);
     res.status(500).json({
       status: 'error',
-      message: 'Something went badly wrong💥'
+      message: 'Something went very wrong💥'
     });
   }
 };
 
 const globalErrorHandler = (err, req, res, next) => {
-  console.log('err :>> ', err);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  err.message = err.message || 'something went wrong';
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+  let error = { ...err };
+  console.log('error :>> ', JSON.parse(JSON.stringify(error)));
+  error.message = err.message;
+  error.name = err.name;
+  error.statusCode = err.statusCode || 500;
+  error.status = err.status || 'error';
+
+  if (err.name === 'CastError') error = handleCastErrorDB(err);
+  if (err.code === 11000) error = handleDuplicateFields(err);
+  if (err.name === 'ValidationError') error = handleValidationError(err);
+
+  if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(error, res);
   } else {
-    sendErrorProd(err, res);
+    sendErrorDev(error, res);
   }
 };
 
